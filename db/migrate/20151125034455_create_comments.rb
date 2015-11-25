81a9bdbbb1eeb5b7de4ea731342e872ca843ac0f
Lexi Tran <hazukitran@gmail.com>
2015-11-25 13:46:38 +0700

    Fix a few bugs on comments

--- db/migrate/20151125034455_create_comments.rb
+++ db/migrate/20151125034455_create_comments.rb
@@ -2,6 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.integer :link_id
+      t.string :body
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20151125034455) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
+    t.string   "body"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
